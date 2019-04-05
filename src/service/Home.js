24b1960d96a6853b087fbad04acd3a721a16458b
rija_randriamanampy <rijara8@gmail.com>
2019-04-05 18:01:23 +0300

    05 04 2019

--- src/service/Home.js
+++ src/service/Home.js
@@ -16,7 +16,7 @@ import {
 export default class Home extends Component {
 
     second_onPressButton() {
-        Alert.alert('wine2')
+        Alert.alert('wine22')
     }
     third_onPressButton() {
         Alert.alert('wine3')
@@ -36,36 +36,36 @@ export default class Home extends Component {
                 showsVerticalScrollIndicator={false}>
                 <View style={styles.container}>
                     <View style={styles.icon}>
-                        <ImageBackground source={require('../img/Entete.png')} style={{ width: wp('30%'),height: hp('10%')}}>
+                        <ImageBackground source={require('../img/Entete.png')} style={{ width: wp('30,53%'),height: 0.6*wp('30,53%')}}>
 
                         </ImageBackground>
                     </View>
                     <View style={styles.row}>
                         <View style={styles.first}>
                             <View style={styles.firstButton}>
-                                <ImageBackground source={require('../img/icon1.png')} style={{ height: hp('25%'),marginTop: 5,paddingTop: 1}}>
+                                <ImageBackground source={require('../img/icon1.png')} style={{ height: 1.08*wp('40,38%'),marginTop: 5,paddingTop: 1}}>
                                     <TouchableOpacity style={{ height: hp('30%')}} onPress={() => this.props.navigation.navigate('Fullwinelist')}>
                                     </TouchableOpacity>
                                 </ImageBackground>
                             </View>
                             <View style={styles.secondButton}>
-                                <ImageBackground source={require('../img/icon2.png')} style={{ height: hp('25%'),marginTop: 5,paddingTop: 1}}>
-                                    <TouchableOpacity style={{ height: hp('30%')}} onPress={() => this.props.navigation.navigate('Glass')}>
+                                <ImageBackground source={require('../img/icon2.png')} style={{ height: 1.08*wp('40,38%'),marginTop: 5,paddingTop: 1}}>
+                                    <TouchableOpacity style={{ height: hp('30%')}} onPress={() => this.props.navigation.navigate('Test')}>
                                     </TouchableOpacity>
                                 </ImageBackground>
                             </View>
                         </View>
                         <View style={styles.second}>
                             <View style={styles.thirdButton}>
-                                <ImageBackground source={require('../img/icon3.png')} style={{ height: hp('25%'), marginTop: 5,paddingTop: 1}}>
-                                    <TouchableOpacity style={{ height: hp('30%')}} onPress={this.third_onPressButton}>
+                                <ImageBackground source={require('../img/icon3.png')} style={{ height: 1.08*wp('40,38%'), marginTop: 5,paddingTop: 1}}>
+                                    <TouchableOpacity style={{ height: hp('30%')}} onPress={() => this.props.navigation.navigate('Halfofflist')}>
                                         <Text>wine3</Text>
                                     </TouchableOpacity>
                                 </ImageBackground>
                             </View>
                             <View style={styles.fourthButton}>
-                                <ImageBackground source={require('../img/icon4.png')} style={{ height: hp('25%'),marginTop: 5,paddingTop: 1}}>
-                                    <TouchableOpacity style={{ height: hp('30%')}} onPress={() => this.props.navigation.navigate('ChampagneBest')}>
+                                <ImageBackground source={require('../img/icon4.png')} style={{ height: 1.08*wp('40,38%'),marginTop: 5,paddingTop: 1}}>
+                                    <TouchableOpacity style={{ height: hp('30%')}} onPress={() => this.props.navigation.navigate('Bestoflist')}>
                                         <Text>wine4</Text>
                                     </TouchableOpacity>
                                 </ImageBackground>
@@ -81,38 +81,40 @@ export default class Home extends Component {
 
 const styles = StyleSheet.create({
     container : {
-        backgroundColor: '#000000'
+        backgroundColor: '#000000',
+        width:wp('100%'),
     },
     icon: {
-        width:wp('35%'),
-        marginTop:50,
-        marginBottom:10,
-        marginLeft:wp('40%'),
+        marginTop:hp("3,8%"),
+        marginBottom:wp("5,46"),
+        alignItems: 'center'
     },
     row:{
         borderWidth: 5,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     first: {
-        marginBottom: 3,
+        marginBottom: hp("2,8%"),
         marginRight:20,
         marginLeft:20,
         flexDirection: 'row',
         justifyContent: 'space-between'
     },
     firstButton: {
-        marginRight: 3,
-        width: wp('45%'),
-        height: hp('30%'),
+        marginRight: wp("2,75%"),
+        width: wp('42,38%'),
+        height: 1.08*wp('42,38%'),
         borderColor:'#00a54f',
-        borderWidth: 5,
+        borderWidth: wp('1,23%'),
         color:'#f1592a',
 
     },
     secondButton:{
-        width: wp('45%'),
-        height: hp('30%'),
+        width: wp('42,38%'),
+        height: 1.08*wp('42,38%'),
         borderColor:'#00a54f',
-        borderWidth: 5,
+        borderWidth: wp('1,23%'),
         color:'#f1592a',
     },
     second:{
@@ -122,18 +124,18 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between'
     },
     thirdButton:{
-        marginRight: 3,
-        width: wp('45%'),
-        height: hp('30%'),
+        marginRight: wp("2,75%"),
+        width: wp('42,38%'),
+        height: 1.09*wp('42,38%'),
         borderColor:'#00a54f',
-        borderWidth: 5,
+        borderWidth: wp('1,23%'),
         color:'#f1592a',
     },
     fourthButton:{
-        width: wp('45%'),
-        height: hp('30%'),
+        width: wp('42,38%'),
+        height: 1.09*wp('42,38%'),
         borderColor:'#dbbb6a',
-        borderWidth: 5,
+        borderWidth: wp('1,23%'),
         color:'white',
     }
 
--- src/service/Test.js
+++ src/service/Test.js
@@ -1,41 +1,89 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, View, ActivityIndicator, Text, Alert} from 'react-native';
-export default class Test extends Component {
-handlePress = async () => {
-  fetch('https://data.advance88.hasura-app.io/v1/query', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-          "type": "select",
-    "args": {
-        "table": "author",
-        "columns": [
-            "name"
-        ],
-        "limit": "1"
-    }
-      })
+import {
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    ImageBackground,
+    ScrollView,
+    Modal,
+    TouchableHighlight,
+    CheckBox,
+    TextInput,
+    FlatList,
+    Linking,
+    ActivityIndicator
+} from 'react-native';
 
-})
-    .then((response) => response.json())
-    .then((responseJson) => {
- Alert.alert("Author name at 0th index:  " + responseJson[0].name);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+import {heightPercentageToDP as hp,
+    widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
+    
+let SQLite = require('react-native-sqlite-storage')
+let db = SQLite.openDatabase({name:'mmb_ipad.db', createFromLocation:'~/database/mmb_ipad.db'})
+export default class Test extends Component {
+constructor(props) {
+    super();
+    this.state = {
+        id: '',
+        type: '',
+        name: '',
+        ActivityIndicator_Loading: false,
+        ipad_countries: [], 
+    }
+    
+    //all wine
+    db.transaction(tx => {
+        tx.executeSql('SELECT * FROM ipad_wines', [], (tx, results) => {
+          var temp = [];
+          for (let i = 0; i < results.rows.length; ++i) {
+            temp.push(results.rows.item(i));
+          }
+          this.setState({
+            ipad_countries: temp,
+          });
+        });
+      });
+    }
+
+    ListViewItemSeparator = () => {
+        return (
+          <View style={{ height: 0.1, width: '100%', backgroundColor: 'black' }} />
+        );
+    };
+
+  render() {
+    return (
+        <View style = { styles.MainContainer }>
+                <ScrollView style={{backgroundColor:'black',width:wp('100%')}}> 
+                
+                <FlatList
+                data={this.state.ipad_countries}
+                ItemSeparatorComponent={this.ListViewItemSeparator}
+                keyExtractor={(item, index) => index.toString()}
+                extraData={this.state}
+                renderItem={({ item }) => (
+                <View key={item.id} style={{borderColor:'#808080', borderBottomWidth: 0.6, marginTop:10}}>
+                    <View style={{flexDirection:'row',justifyContent: 'space-between'}}>
+                        <Text style={{fontSize:30, textAlign: 'center',color:'#fff', height:wp('9.65%')}}>{item.name}</Text>
+                    </View>
+                </View>
+                    )}
+                    />
+                </ScrollView>
+        </View>
+    );
+  }
 }
-  render(){
-  return(
-   <View style={{paddingTop: 50, paddingLeft: 50 }}>
-   <Text> Some other text </Text>
-    <Text> Some other text </Text>
-    <TouchableOpacity onPress={this.handlePress.bind(this)}>
-     <Text style={{paddingTop: 50, paddingLeft: 50, color: '#FF0000'}}> Click me to see the name </Text>
-    </TouchableOpacity>
-</View> 
-  );
-}
-}
+
+const styles = StyleSheet.create(
+    {
+        MainContainer:
+        {
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          margin: 20
+     
+        }
+});
